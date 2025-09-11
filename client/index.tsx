194437b7b409f281756f811466d1317cfbd8ddf6
Rhaine <215462474+rhainem@users.noreply.github.com>
2025-09-11 15:53:35 +1200

    add auth0 authentication (login & logout)

--- client/index.tsx
+++ client/index.tsx
@@ -11,15 +11,12 @@ const queryClient = new QueryClient()
 
 document.addEventListener('DOMContentLoaded', () => {
   createRoot(document.getElementById('app') as HTMLElement).render(
-    /**
-     * TODO: replace domain, clientId, and audience
-     */
     <Auth0Provider
-      domain=""
-      clientId=""
+      domain="rhaine.au.auth0.com"
+      clientId="FhPjfk30gjwr3oaEM6RFlDkNCi8Tszy2"
       authorizationParams={{
         redirect_uri: window.location.origin,
-        audience: '',
+        audience: 'https://tailtrail/api',
       }}
     >
       <QueryClientProvider client={queryClient}>
