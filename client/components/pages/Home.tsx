01fc6f947dbf759e290713991dc098df3b94d6c7
Irina Kostina <irina.kostina.nz@gmail.com>
2025-09-12 12:49:22 +1200

    Add LostFounfButton filters

--- client/components/pages/Home.tsx
+++ client/components/pages/Home.tsx
@@ -1,7 +1,33 @@
+import { useState, useEffect } from 'react'
 import MapComponent from '../map/MapComponent'
 import ListOfPets from '../pets/ListOfPets'
+import type { Pet } from '../../../models/pet'
+import LostFoundButton from './LostFoundButton'
 
 export default function HomePage() {
+  // Filter state and pet state
+  const [filter, setFilter] = useState<'all' | 'lost' | 'found'>('all')
+  const [pets, setPets] = useState<Pet[]>([])
+
+  // Get pets from the API when the component appears 
+  useEffect(() => {
+    async function fetchPets() {
+      const res = await fetch('/api/v1/pets')
+      const data: Pet[] = await res.json()
+      setPets(data)
+    }
+    fetchPets()
+  }, [])
+
+  // Apply filtering logic
+const filteredPets = pets.filter((pet) => {
+  const lostValue = String(pet.lost).toLowerCase()
+
+  if (filter === 'lost') return lostValue === 'true'
+  if (filter === 'found') return lostValue === 'false'
+  return true
+})
+
   return (
     <>
     <h1 className='text-2xl font-bold mb-4'>Welcome to TailTrail</h1>
@@ -23,19 +49,21 @@ export default function HomePage() {
 
         <div className="mt-6">
           <h3 className="font-semibold">Recent Logs</h3>
-          <ListOfPets />
+          <ListOfPets pets={filteredPets} />
         </div>
       </aside>
 
       {/* RIGHT COLUMN */}
       {/* Map as a main component */}
       <main className="w-[60%] p-4 relative">
-        <MapComponent />
+        <LostFoundButton filter={filter} setFilter={setFilter} />
+        <MapComponent /> 
+        {/* pets={filteredPets} Add this to mapcomponent when map is ready */}
 
-        <div className="absolute top-4 right-4 flex gap-2">
+        {/* <div className="absolute top-4 right-4 flex gap-2">
           <button className="bg-red-600 text-white px-4 py-2 rounded">LOST</button>
           <button className="bg-green-500 text-white px-4 py-2 rounded">FOUND</button>
-        </div>
+        </div> */}
       </main>
     </div>
     </>
